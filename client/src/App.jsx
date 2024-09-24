404e79eb64aab501cba4a853ac50b6a736df3672
MitaliM13 <mitalimaurya@kccemsr.edu.in>
2024-09-24 12:40:06 +0530

    update

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,54 @@
-
+import { useEffect, useState, useMemo } from "react"
+import {io} from "socket.io-client"
+import { Container, Typography, TextField, Button } from "@mui/material"
 
 function App() {
 
+  const socket = useMemo(() => io("http://localhost:3000"), [])
+
+  const [message, setMessage] = useState("")
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    socket.emit("message", message)
+    setMessage("")
+  }
+
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log('connected', socket.id)
+
+      socket.on("message", (msg) => {
+        console.log(msg )
+      })
+      
+      socket.on('received', (data) => {
+        console.log(data)
+      })
+
+      return () => {
+        socket.disconnect()
+      }
+    })
+  }, [])
+
   return (
-    <>
-      <div className="text-3xl text-center">CHAT ROOM</div>
-    </>
+      
+      <Container maxWidth="sm">
+        <Typography variant="h1" component="div">Chat Room</Typography>
+
+        <form onSubmit={handleSubmit}>
+          <TextField 
+            id="outlined-basic" 
+            label="Outlined" 
+            variant="outlined" 
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}  
+          />
+          <Button variant="contained" color="primary" type="submit">Send</Button>
+        </form>
+      </Container>
+           
   )
 }
 
--- server/app.js
+++ server/app.js
@@ -1,22 +1,40 @@
 import express from 'express'
 import { Server } from 'socket.io'
 import { createServer } from 'http'
+import cors from 'cors'
 
 const app = express()
 const PORT = 3000
 
 const server = createServer(app)
 
-const io = new Server(server)
+const io = new Server(server, {
+    cors: {
+        origin: 'http://localhost:5173',
+        methods: ['GET', 'POST'],
+        credentials: true
+    }
+})
+
+app.use(cors())
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World!') 
 })
 
 io.on('connection', (socket) => {
     console.log(`User connected: ${socket.id}`)
+
+    socket.on("message", (data) => {
+        console.log(data)
+        io.emit("received", data)
+    })
+
+    socket.on('disconnect', () => {
+        console.log(`User disconnected: ${socket.id}`)
+    })
 })
 
-app.listen(PORT, () => {
+server.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
